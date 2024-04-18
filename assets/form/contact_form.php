0b3b33aae4a374a7c45a7405643250f15f82a973
Imad-Ocreateo <imad@ocreateo.com>
2024-04-18 22:11:33 +0500

    test

--- assets/form/contact_form.php
+++ assets/form/contact_form.php
@@ -3,16 +3,12 @@
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
   $name = $_POST["name"];
   $email = $_POST["email"];
-  // Retrieve other form fields as needed
-  
-  // Construct email message
+
   $to = "imad@ocreateo.com";
   $subject = "New Feedback Submission";
   $message = "Name: $name\n";
   $message .= "Email: $email\n";
-  // Include other form fields in the message as needed
 
-  // Send email
   $headers = "From: $email\r\n";
   if (mail($to, $subject, $message, $headers)) {
     echo json_encode(array("message" => "Email sent successfully"));
